abc150d381e4dc3152dfefaebad3bab9347e5cc7
juliajos218 <juliajos@umich.edu>
2026-08-12 23:39:50 -0400

    Change sector range in download_lists.sh
    Updated the sector range from 70-83 to 61-69 in the download script.

--- sh_files/download_lists.sh
+++ sh_files/download_lists.sh
@@ -2,7 +2,7 @@
 # download_all_targets.sh
 # Fetch sector target-list text files S070–S083 (keeps existing files).
 
-for s in $(seq -f "%03g" 70 83); do        # 070 … 083
+for s in $(seq -f "%03g" 61 69); do        # 061 … 069 change sector values here
     url="https://tess.mit.edu/public/target_lists/2m/all_targets_S${s}_v1.txt"
     wget -nc "$url"                        # -nc = “no clobber” (skip if already there)
 done
